e3ca511f99eeb9ec74eaa9d11ae2a4c6c6b5c768
Hannah De Los Reyes <htdelosreyes8@gmail.com>
2018-09-18 13:15:25 -0700

    first commit

---
+++ shot-timer.js
@@ -0,0 +1,15 @@
+function getRandom(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function playSound() {
+  new Audio("beep.mp3").play();
+}
+
+let int = getRandom(3, 7) * 1000;
+const startButton = document.querySelector("button");
+
+
+startButton.addEventListener('click', function () {
+  setTimeout(playSound, int);
+});
