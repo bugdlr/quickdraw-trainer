683c2e3af3e051ccec534f1703df36550cb3bf21
Hannah De Los Reyes <htdelosreyes8@gmail.com>
2018-09-19 14:39:11 -0700

    changed interval

--- shot-timer.js
+++ shot-timer.js
@@ -1,7 +1,6 @@
 const audio = document.querySelector("audio");
 const startButton = document.querySelector("button");
 
-
 function getRandom(min, max) {
   return Math.random() * (max - min) + min;
 }
@@ -11,10 +10,9 @@ function playSound() {
 }
 
 function onClick() {
-  let int = getRandom(3, 6) * 1000;
+  let int = getRandom(3, 8) * 1000;
   console.log(int);
   setTimeout(playSound, int);
 }
-
 
 startButton.addEventListener('click', onClick, false);
